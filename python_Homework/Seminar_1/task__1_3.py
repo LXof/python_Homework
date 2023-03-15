422efe60a239e1b35e4bd6c71e4ad832a6e6fbf6
ples <plescov01@gmail.com>
2023-03-15 04:00:34 +0300

    feat: task__1_3 - implement 2 solutions

--- python_Homework/Seminar_1/task__1_3.py
+++ python_Homework/Seminar_1/task__1_3.py
@@ -11,3 +11,21 @@
 # 123456 >>> no
 
 # (*) Усложнение. Вывод результат на экран сделайте одной строкой(только один print), для этого используйте тернарный оператор
+
+
+# Решение со срезами
+transport_Ticket = '385916'
+
+condition_1 = int(transport_Ticket[0]) + int(transport_Ticket[1]) + int(transport_Ticket[2])
+condition_2 = int(transport_Ticket[3]) + int(transport_Ticket[4]) + int(transport_Ticket[5])
+
+print(f"{transport_Ticket} >>> {'yes' if condition_1 == condition_2 else 'no'}")
+
+# Решение с математикой.
+transport_Ticket = 385916
+
+condition_1 = ((transport_Ticket // 1000) // 100) + (((transport_Ticket // 1000) // 10) % 10) + ((transport_Ticket // 1000) % 10)
+condition_2 = ((transport_Ticket % 1000) // 100) + (((transport_Ticket % 1000) // 10) % 10) + ((transport_Ticket % 1000) % 10)
+
+print(f"{transport_Ticket} >>> {'yes' if condition_1 == condition_2 else 'no'}")
+
